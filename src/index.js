ce90908fe3bb0765a16216aa91452896e66ed928
Pivotass <pivotass@MacBook-Pro-de-Pivotass.local>
2023-04-13 16:41:28 +0200

    feat: 🎸 Added css variables for each shades name

--- src/index.js
+++ src/index.js
@@ -11,9 +11,29 @@ exports.palettes = async (args = {}) => {
 	return {
 		name: "tailwind-palettes-generator",
 		async config(config) {
-			let scss = "";
+			let scss = ":root {\n";
 			let keys = Object.entries(args);
 			let [defaultPalette] = keys[0] || [];
+
+			for (const [key, hex] of keys) {
+				const shades = generateTailwindColorFamily(hex, tailwindColors3);
+
+				shades.forEach((shade) => {
+					scss += `\t--${key}-${shade.number}: ${shade.hexcode};\n`;
+				});
+
+				scss += "\n";
+
+				shades.forEach((shade) => {
+					scss += `\t--on-${key}-${shade.number}: ${
+						shade.luminance < 40 ? `var(--${key}-100)` : `var(--${key}-900)`
+					};\n`;
+				});
+
+				scss += "\n";
+			}
+
+			scss += "\n}\n\n";
 
 			for (const [key, hex] of keys) {
 				const shades = generateTailwindColorFamily(hex, tailwindColors3);
@@ -21,15 +41,13 @@ exports.palettes = async (args = {}) => {
 				scss += `${key === defaultPalette ? `:root,\n.${key}` : `.${key}`} {\n`;
 
 				shades.forEach((shade) => {
-					scss += `\t--color-${shade.number}: ${shade.hexcode};\n`;
+					scss += `\t--color-${shade.number}: var(--${key}-${shade.number});\n`;
 				});
 
 				scss += "\n";
 
 				shades.forEach((shade) => {
-					scss += `\t--on-color-${shade.number}: ${
-						shade.luminance < 40 ? "var(--color-100)" : "var(--color-900)"
-					};\n`;
+					scss += `\t--on-color-${shade.number}: var(--on-${key}-${shade.number});\n`;
 				});
 
 				scss += "}\n\n";
